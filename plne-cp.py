341eaeba017889b07f1c0292030871614975ad4e
constant-gayet <76485344+constant-gayet@users.noreply.github.com>
2022-11-25 17:48:47 +0100

    adding constraints to problem 2

--- plne-cp.py
+++ plne-cp.py
@@ -1,16 +1,18 @@
 # Formulation en programme linéaire en nombres entiers basée sur le concept de flot (PLNE-CP) [CGI09]
 
-from pulp import *
+import pulp as pl
 import networkx as nx
 
 # Problem
-problem = LpProblem("CGI09", LpMinimize)
 
 
 # Variables
-def create_variables(graph):
+def define_problem(graph):
+    problem = pl.LpProblem("CGI09", pl.LpMinimize)
     # ensemble de noeuds
     nodes = graph.nodes
+    nodes_s = nodes[1:] # ensemble des noeuds privés de la source
+    source = nodes[1]
 
     # ensemble d'aretes
     edges = graph.edges
@@ -20,10 +22,29 @@ def create_variables(graph):
     # ensembes d'arcs
     arcs = directed_graph.edges
 
-    x = []
-    x = [LpVariable("x" + arc[0] + arc[1], 0, 1, cat=LpInteger) for arc in arcs]
+    # On créé nos variables xuv et yv
 
-    y = []
-    y = [LpVariable("y" + node.value, 0, 1, cat="Integer") for node in nodes]
+    x = pl.LpVariable.dicts("x", arcs, 0, 1, cat=pl.LpBinary)
+    y = pl.LpVariable.dicts("y", nodes, 0, 1, cat=pl.LpBinary)
 
+    # constraint 9
+    for x in nodes:
+        tmplist = []
+        for u in directed_graph.in_edges(x):
+            tmplist.append(u)
+        in_arcs = pl.LpVariable.dicts("A_moins", tmplist, 0, 1, cat=pl.LpBinary)
+        problem += pl.lpSum(in_arcs) == 1
 
+    # constraint 10
+    tmplist = []
+    tmplist2 = []
+    for v in directed_graph.out_edges(source):
+        tmplist.append(v)
+    for u in directed_graph.in_edges(source):
+        tmplist2.append(u)
+    f_sv = pl.LpVariable.dicts("flows_sv", tmplist, 0, 1, cat=pl.LpBinary)
+    f_uv = pl.LpVariable.dicts("flows_uv", tmplist2, 0, 1, cat=pl.LpBinary)
+
+    problem += (pl.lpSum(f_sv) - pl.lpSum(f_uv)) <= (directed_graph.number_of_nodes() - 1)
+
+    # constraint 11
